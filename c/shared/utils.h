cfabd5ce664fc5d54d1033a3383a12a8f33cb2ba
Felipe Coimbra <felipecoimbra97@gmail.com>
2022-06-10 18:17:00 -0300

    Add error helpers from book

--- c/shared/utils.h
+++ c/shared/utils.h
@@ -1,6 +1,20 @@
 #ifndef __SHARED_UTILS_H__
 #define __SHARED_UTILS_H__
 
+#include <sys/types.h> /* Type definitions used by many programs */
+#include <stdio.h> /* Standard I/O functions */
+#include <stdlib.h> /* Prototypes of commonly used library functions,
+plus EXIT_SUCCESS and EXIT_FAILURE constants */
+#include <unistd.h> /* Prototypes for many system calls */
+#include <errno.h> /* Declares errno and defines error constants */
+#include <string.h> /* Commonly used string-handling functions */
+#include "errors.h" /* Declares our error-handling functions */
+
+typedef enum { FALSE, TRUE } Boolean;
+
+#define min(m,n) ((m) < (n) ? (m) : (n))
+#define max(m,n) ((m) > (n) ? (m) : (n))
+
 int cmp_question(const char* candidate, int question_number);
 
 #endif
